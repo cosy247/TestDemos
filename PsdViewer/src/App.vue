36c40604e0bc7ccdb0e824340580a8add0175b0a
cosy247 <705194673@qq.com>
2025-04-09 16:11:05 +0800

    用Vue 3和Vite初始化PsdViewer项目

---
+++ PsdViewer/src/App.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="container">
+    <div id="dropzone">
+      <p>Drop here</p>
+    </div>
+    <div id="content">
+      <div ref="imageRef" id="image"></div>
+      <pre ref="dataRef" id="data"></pre>
+    </div>
+    <input type="file" @change="handleFileChange" />
+  </div>
+</template>
+
+<script setup>
+import { onMounted, ref } from 'vue';
+import PSD from '@n.see/psd-parser';
+
+const dataRef = ref();
+const imageRef = ref();
+
+function handleFileChange(event) {
+  const file = event.target.files[0];
+  const url = URL.createObjectURL(file);
+  PSD.fromURL(url).then((psd) => {
+    console.log('psd==========', psd);
+    const data = psd.tree().export();
+    const dataString = JSON.stringify(data, undefined, 2);
+    if (dataRef.value) {
+      dataRef.value.innerHTML = dataString;
+    }
+    const img = psd.image.toPng();
+    if (imageRef.value) {
+      imageRef.value.appendChild(img);
+    }
+
+    const node = psd.tree();
+    console.log('node', node);
+  });
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+}
+#data {
+  background-color: #eee;
+  padding: 10px;
+  margin: 0;
+  flex: 1;
+}
+
+#dropzone {
+  width: 500px;
+  height: 100px;
+  border: 1px #ababab dashed;
+  margin: 50px auto;
+}
+
+#dropzone p {
+  text-align: center;
+  line-height: 100px;
+  margin: 0;
+  padding: 0;
+  background-color: #eee;
+}
+
+#content {
+  display: flex;
+}
+</style>
