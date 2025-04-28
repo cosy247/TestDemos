9ffc787a47029e8fa29b16940b4ea022dd23a363
cosy247 <705194673@qq.com>
2025-04-28 10:26:23 +0800

    chore: add VitePress configuration with initial scripts and dependencies

---
+++ VitePress/.vitepress/config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "BlogByCosy",
+  description: "A VitePress Site"
+})
